6b4c1f2b026526d2b52d949384dabc2eb2c42526
TonyTroev <tony13troev@gmail.com>
2018-03-09 21:10:15 +0200

    Adding The Third Problem

---
+++ intro-and-basic-syntax/Problem 3/MilesToKilometres.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MilesToKilometres
+{
+    class MilesToKilometres
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{input * 1.60934:F2}");
+        }
+    }
+}
